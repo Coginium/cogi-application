43e41f5b326e7286938573ae552338939b796a87
Pawel <pawel.kuznik@gmail.com>
2022-01-10 10:55:17 +0100

    Documentation

--- src/Catalogue.tsx
+++ src/Catalogue.tsx
@@ -1,7 +1,15 @@
 import List from "./Catalogue/List";
 
+/**
+ *  This is a component that can be used to manage a catalogue of possible products:
+ *  models, boxes, etc. These products aren't yet in the collection and are available
+ *  for all users for purchuase.
+ * 
+ *  Note: possibly we need a better way to deal with source of data for this section.
+ *  Ideally, it would auto-update, not require maintenance, and be always in
+ *  a valid state. 
+ */
 export default function Catalogue() {
-
 
     return (
         <>
