cc62d8e207e6e818577da082216956d9f30a357a
Pawel <pawel.kuznik@gmail.com>
2022-01-21 00:56:17 +0100

    a little bit of doc for future

---
+++ src/AddModels/modelsSlice.ts
@@ -0,0 +1,6 @@
+/**
+ *  This file will contain a Redux slice for models that should be added. This will
+ *  be the shared state between the parent component and the components dealing with
+ *  specific model decistions.
+ */
+export default null;
