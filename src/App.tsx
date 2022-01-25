93a8b1d73ac1341c5406153856cd9056e119e7ad
Pawel <pawel.kuznik@gmail.com>
2022-01-26 00:37:05 +0100

    Switched to hash router so we can deploy to GitPages

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Collection from './Collection';
 import { Provider as ReduxProvider } from 'react-redux'; 
 import store from './Store';
 import Catalogue from './Catalogue';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import Frame from './Frame';
 import Model from './Model';
 import AddModels from './AddModels';
@@ -12,16 +12,16 @@ import AddModels from './AddModels';
 function App() {
   return (
       <ReduxProvider store={store}>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
-            <Route path="/" element={<Frame/>}>
+            <Route path="*" element={<Frame/>}>
               <Route path="catalogue" element={<Catalogue/>}/>
               <Route path="collection" element={<Collection/>}/>              
               <Route path="model/:model" element={<Model/>}/>
               <Route path="new-model/:type/:id" element={<AddModels/>}/>
             </Route>
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </ReduxProvider>
   );
 }
